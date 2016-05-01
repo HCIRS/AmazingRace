87fc46427680252bbbeaa35373a525075e65d57a
jiefeng <hjf3108@gmail.com>
2016-05-01 06:41:31 +0000

    Changed table column names

--- models/entry.js
+++ models/entry.js
@@ -3,18 +3,18 @@ var mysql = require('../models/mysql').getConnection()
 var entry = {};
 
 entry.insert = function(id, flagid, callback) {
-	mysql.query('INSERT INTO entries (flagid, participantid) VALUES ( ?, ? )', [flagid, id] , callback);
+	mysql.query('INSERT INTO entries (flag, participant) VALUES ( ?, ? )', [flagid, id] , callback);
 }
 
 entry.getRanks = function(callback) {
-	mysql.query('SELECT participantid, sum(points) AS totalpoints FROM \
+	mysql.query('SELECT participant, sum(points) AS totalpoints FROM \
 				( \
-					SELECT participantid, points FROM entries \
+					SELECT participant, points FROM entries \
 					LEFT JOIN flags \
-					ON entries.flagid = flags.id \
-					GROUP BY participantid, flagid \
+					ON entries.flag = flags.id \
+					GROUP BY participant, flag \
 				) scores \
-				GROUP BY participantid \
+				GROUP BY participant \
 				ORDER BY totalpoints DESC', callback);
 }
 
